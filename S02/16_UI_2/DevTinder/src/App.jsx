38d7aa2f47321433b0f9f09d3ce540f02dc29733
singhvi1 <singvi182003@gmail.com>
2025-10-25 10:25:00 +0530

    "16_UI_start"

---
+++ S02/16_UI_2/DevTinder/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
